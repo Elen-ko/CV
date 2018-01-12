9892052010d865505a7550221c1e2278bd59b18e
Alexey Kovtunets <alexey.kovtunets@kartenmacherei.de>
2018-01-12 23:16:22 +0100

    Added html watcher. Cleanup.

--- gulpfile.js
+++ gulpfile.js
@@ -1,94 +1,89 @@
 // include necassary gulp plugins for the project
-var gulp = 				require('gulp'),
-	concat = 			require('gulp-concat'),
-	imageop = require('gulp-image-optimization'),
-	sass =				require('gulp-sass'),
-	minifyCSS =			require('gulp-minify-css'),
-	plumber = 			require('gulp-plumber'),
-	rename = 			require('gulp-rename'),
-	uglify = 			require('gulp-uglify'),
-	//iconfont =        require('gulp-iconfont'),
-	livereload =        require('gulp-livereload'),
-  runTimestamp = Math.round(Date.now()/1000);
+var gulp = require("gulp"),
+    concat = require("gulp-concat"),
+    imageop = require("gulp-image-optimization"),
+    sass = require("gulp-sass"),
+    minifyCSS = require("gulp-minify-css"),
+    plumber = require("gulp-plumber"),
+    rename = require("gulp-rename"),
+    uglify = require("gulp-uglify"),
+    livereload = require("gulp-livereload"),
+    runTimestamp = Math.round(Date.now() / 1000);
 
 // error handling
 var onError = function(err) {
-	    console.log(err);
-	}
+    console.log(err);
+};
 
 // defining paths and environmental variables
 var bases = {
-		dev		: 'resources/',
-		live 	: 'public/'
-	},
-	paths = {
-		css 	: 'css/',
-		img 	: 'img/',
-		js 		: 'js/',
-		sass 	: 'sass/',
-    fonts   : 'fonts/'
-	};
+        dev: "resources/",
+        live: "public/"
+    },
+    paths = {
+        css: "css/",
+        img: "img/",
+        js: "js/",
+        sass: "sass/",
+        fonts: "fonts/"
+    };
 
-
-gulp.task('styles', function () {
-	gulp.src(['styles.scss'], {cwd: bases.dev + paths.sass})
-	.pipe(plumber({
-        errorHandler: onError
-    }))
-	.pipe(sass())
-	.pipe(concat('styles.css'))
-	.pipe(rename({suffix: '.min'}))
-	.pipe(minifyCSS())
-	.pipe(gulp.dest(bases.live + paths.css));
+gulp.task("styles", function() {
+    gulp
+        .src(["styles.scss"], { cwd: bases.dev + paths.sass })
+        .pipe(
+            plumber({
+                errorHandler: onError
+            })
+        )
+        .pipe(sass())
+        .pipe(concat("styles.css"))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(minifyCSS())
+        .pipe(gulp.dest(bases.live + paths.css));
 });
 
-gulp.task('images', function(cb) {
-    gulp.src(['resources/img/*.png','resources/img/*.jpg','resources/img/*.gif','resources/img/*.jpeg']).pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    })).pipe(gulp.dest(bases.live + paths.img)).on('end', cb).on('error', cb);
+gulp.task("images", function(cb) {
+    gulp
+        .src([
+            bases.dev + "img/*.png",
+            bases.dev + "img/*.jpg",
+            bases.dev + "img/*.gif",
+            bases.dev + "img/*.jpeg"
+        ])
+        .pipe(
+            imageop({
+                optimizationLevel: 5,
+                progressive: true,
+                interlaced: true
+            })
+        )
+        .pipe(gulp.dest(bases.live + paths.img))
+        .on("end", cb)
+        .on("error", cb);
 });
 
-gulp.task('js', function() {
-	gulp.src("resources/js/*.js")
-		.pipe(concat('scripts.js'))
-		.pipe(rename({suffix: '.min'}))
-    	.pipe(uglify())
-    	.pipe(gulp.dest(bases.live + paths.js));
+gulp.task("js", function() {
+    gulp
+        .src(bases.dev + "js/*.js")
+        .pipe(concat("scripts.js"))
+        .pipe(rename({ suffix: ".min" }))
+        .pipe(uglify())
+        .pipe(gulp.dest(bases.live + paths.js));
 });
 
-gulp.task('html', function() {
-	gulp.src("resources/*.html")
-    	.pipe(gulp.dest(bases.live));
+gulp.task("html", function() {
+    gulp.src("resources/*.html").pipe(gulp.dest(bases.live));
 });
 
-
-/*gulp.task('iconFont', function(){
-  gulp.src('./resources/icons/*.svg')
-  .pipe(plumber())
-    .pipe(iconfont({
-      fontName: 'futterpedia',
-      appendCodepoints: true,
-      formats: ['ttf', 'eot', 'woff'],
-      timestamp: runTimestamp
-    }))
-      .on('glyphs', function(glyphs, options) {
-          console.log(glyphs, options);
-      })
-    .pipe(gulp.dest(bases.live + paths.fonts));
-});
-*/
-
-gulp.task('watch', function() {
-	gulp.watch(bases.dev + paths.img + '**/*', ['images']);
-	gulp.watch(bases.dev + paths.sass + '**/*.scss', ['styles']);
-	//gulp.watch(bases.dev + paths.js + '**/*.js', ['js']);
-    //gulp.watch(bases.dev + 'icons/*.svg', ['iconFont']);
+gulp.task("watch", function() {
+    gulp.watch(bases.dev + paths.img + "**/*", ["images"]);
+    gulp.watch(bases.dev + paths.sass + "**/*.scss", ["styles"]);
+    gulp.watch(bases.dev + paths.js + "**/*.js", ["js"]);
+    gulp.watch(bases.dev + "*.html", ["html"]);
 
     livereload.listen();
-    gulp.watch(["resources/**/*", "*.html"]).on('change', livereload.changed);
-
+    gulp.watch([bases.dev + "**/*", "*.html"]).on("change", livereload.changed);
 });
 
-gulp.task('default', [ 'html', 'js', 'styles', 'watch']);
+gulp.task("default", ["html", "js", "styles", "watch"]);
